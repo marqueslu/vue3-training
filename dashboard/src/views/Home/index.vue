ed662001d297eef0e706fc06a17e29398ac33403
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-05 07:42:38 -0300

    Add home page

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,45 @@
 <template>
- <h1 class="font-black text-brand-main">Aqui é a home</h1>
+ <custom-header
+  @create-account="handleAccountCreate"
+  @login="handleLogin"
+ />
+ <contact />
+
+<footer class="flex justify-center py-10 bg-brand-gray">
+ <p class="font-medium text-center text-gray-800">feedbacker &copy; 2021</p>
+</footer>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
 
 export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedback' })
+      }
+    })
+
+    function handleLogin () {
+
+    }
+
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
+
 </script>
