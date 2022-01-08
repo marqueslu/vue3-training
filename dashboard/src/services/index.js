d160ade6a8edd31d784e61e67d190a5ba0b2360c
Lucas Marques <lucas.msilva09@gmail.com>
2022-01-08 16:28:09 -0300

    Configure axios and auth service

---
+++ dashboard/src/services/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import AuthService from './auth'
+
+const API_ENVS = {
+  production: '',
+  development: '',
+  local: 'http://localhost:3000'
+}
+
+const httpClient = axios.create({
+  baseURL: API_ENVS.local
+})
+
+httpClient.interceptors.response.use((response) => response, (error) => {
+  console.log(error)
+  const canThrowAnError = error.request.status === 0 ||
+    error.request.status === 500
+
+  if (canThrowAnError) {
+    throw new Error(error.message)
+  }
+
+  return error
+})
+
+export default {
+  auth: AuthService(httpClient)
+}
