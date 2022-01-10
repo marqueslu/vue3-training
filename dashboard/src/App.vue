7fedd80a43da6293d9e1ebcfefe6e78f21d0dcad
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-10 18:12:12 -0300

    Add validation for user token

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,9 +4,28 @@
 </template>
 
 <script>
+import { watch } from 'vue'
 import ModalFactory from './components/ModalFactory'
+import { useRouter, useRoute } from 'vue-router'
+import services from './services'
 
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+
+        if (!token) {
+          router.push({ name: 'Home' })
+        }
+
+        await services.users.getMe()
+      }
+    })
+  }
 }
 </script>
--- dashboard/src/services/index.js
+++ dashboard/src/services/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import router from '../router'
 import AuthService from './auth'
-
+import UsersService from './users'
 const API_ENVS = {
   production: '',
   development: '',
@@ -9,6 +10,15 @@ const API_ENVS = {
 
 const httpClient = axios.create({
   baseURL: API_ENVS.local
+})
+
+httpClient.interceptors.request.use(config => {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    config.headers.common.Authorizarion = `Bearer ${token}`
+  }
+
+  return config
 })
 
 httpClient.interceptors.response.use((response) => response, (error) => {
@@ -20,9 +30,14 @@ httpClient.interceptors.response.use((response) => response, (error) => {
     throw new Error(error.message)
   }
 
+  if (error.response.status === 401) {
+    router.push({ name: 'Home' })
+  }
+
   return error
 })
 
 export default {
-  auth: AuthService(httpClient)
+  auth: AuthService(httpClient),
+  users: UsersService(httpClient)
 }
