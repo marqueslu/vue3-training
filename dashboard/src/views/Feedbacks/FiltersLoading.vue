d92d9036f9a1ca61c112c3f89643c856ce2c7275
Lucas Marques <lucas.msilva09@gmail.com>
2022-01-15 18:48:59 -0300

    Add Filter component

---
+++ dashboard/src/views/Feedbacks/FiltersLoading.vue
@@ -0,0 +1,39 @@
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded"
+    width="100%"
+    height="200px"
+  >
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="50px"
+      animation-duration="2s"
+    />
+ <content-loader
+      class="mt-3 rounded"
+      width="80%"
+      height="50px"
+      animation-duration="2.3s"
+    />
+     <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="50px"
+      animation-duration="2.6s"
+    />
+     <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="50px"
+      animation-duration="2.9s"
+    />
+  </content-loader>
+</template>
+
+<script>
+import ContentLoader from '../../components/ContentLoader'
+export default {
+  components: { ContentLoader }
+}
+</script>
--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -1,13 +1,48 @@
 <template>
-  <h1>Hello Feedback</h1>
+  <div class="div flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Feedbacks
+    </h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Detalhes de todos os feedbacks recebidos
+    </p>
+  </div>
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-10 grid grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black brand-darkgray">
+          Listagem
+        </h1>
+        <suspense>
+          <template #default>
+            <filters  class="mt-8 animate__animated animate__fadeIn animate__faster" />
+          </template>
+          <template #fallback>
+            <filters-loading class="mt-8" />
+          </template>
+        </suspense>
+
+      </div>
+      <div class="col-span-3 px-10 pt-20">
+
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import Filters from './Filters'
+import FiltersLoading from './FiltersLoading'
+import HeaderLogged from '../../components/HeaderLogged'
+
 export default {
+  components: { Filters, FiltersLoading, HeaderLogged },
+  setup () {}
 
 }
 </script>
-
-<style>
-
-</style>
