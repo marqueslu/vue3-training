02ad6de1feb73618bc295420e435d33e287434bb
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-20 12:02:30 -0300

    Initial project configuration

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+import 'animate.css'
+
 createApp(App).mount('#app')
