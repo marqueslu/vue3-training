859ec6974919dfa30ff1b489fcca3f94010ae30a
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-20 18:52:55 -0300

    Update main.ts to show widget

--- widget/src/App.vue
+++ widget/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <h1>Widget</h1>
+  <widget />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Widget from './views/Widget/index.vue'
 
-export default defineComponent({})
+export default defineComponent({
+  components: { Widget }
+})
 </script>
--- widget/src/main.ts
+++ widget/src/main.ts
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
+import Playground from './views/Playground/index.vue'
 import App from './App.vue'
+
+import { setup } from './utils/bootstrap'
 
 import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
 import 'animate.css'
 
-createApp(App).mount('#app')
+setup({
+  onProduction: () => {
+    createApp(App).mount('#app')
+  },
+  onDevelopment: () => {
+    createApp(Playground).mount('#app')
+  }
+})
