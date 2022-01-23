48182d69546e7dc7e106b19e0cc6055ff0ad837e
Lucas Marques <lucas.msilva09@gmail.com>
2022-01-23 13:34:46 -0300

    Add Arrow Right and Close icons

--- widget/src/components/Icon/index.vue
+++ widget/src/components/Icon/index.vue
@@ -7,9 +7,11 @@ import Loading from './Loading.vue'
 import Copy from './Copy.vue'
 import ChevronDown from './ChevronDown.vue'
 import Chat from './Chat.vue'
+import Close from './Close.vue'
+import ArrowRight from './ArrowRight.vue'
 
 export default {
-  components: { Loading, Copy, ChevronDown, Chat },
+  components: { Loading, Copy, ChevronDown, Chat, Close, ArrowRight },
   props: {
     name: { type: String, required: true }
   }
