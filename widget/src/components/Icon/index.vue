6d1f637978523fb658a8bf0da9ebe325b211988d
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-24 18:27:54 -0300

    Add atention and check icons

--- widget/src/components/Icon/index.vue
+++ widget/src/components/Icon/index.vue
@@ -9,9 +9,20 @@ import ChevronDown from './ChevronDown.vue'
 import Chat from './Chat.vue'
 import Close from './Close.vue'
 import ArrowRight from './ArrowRight.vue'
+import Check from './Check.vue'
+import Atention from './Atention.vue'
 
 export default {
-  components: { Loading, Copy, ChevronDown, Chat, Close, ArrowRight },
+  components: {
+    Loading,
+    Copy,
+    ChevronDown,
+    Chat,
+    Close,
+    ArrowRight,
+    Check,
+    Atention
+  },
   props: {
     name: { type: String, required: true }
   }
