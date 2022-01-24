cb358089064d58cd5ff02d1958bf91ced38fbdd0
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-24 18:29:28 -0300

    Add wizard component

--- widget/src/components/Wizard/index.vue
+++ widget/src/components/Wizard/index.vue
@@ -9,7 +9,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import SelectFeedbackType from './SelectFeedbackType.vue'
-import WriteAFeedback from '../Wizard/WriteAFeedback.vue'
+import WriteAFeedback from './WriteAFeedback.vue'
+import Success from './Success.vue'
+import ErrorState from './Error.vue'
+
 import useStore from '../../hooks/store'
 import useNavigation, { Navigation } from '@/hooks/navigation'
 import { StoreState, setFeedbackType } from '../../store'
@@ -21,7 +24,7 @@ interface SetupReturn {
 }
 
 export default defineComponent({
-  components: { SelectFeedbackType, WriteAFeedback },
+  components: { SelectFeedbackType, WriteAFeedback, Success, Error: ErrorState },
   setup (): SetupReturn {
     const store = useStore()
     const { next } = useNavigation()
