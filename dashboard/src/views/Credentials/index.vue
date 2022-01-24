74f78d23458bd10549054797673b7809c8f0750c
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-24 18:26:16 -0300

    Update widgtet tag script

--- dashboard/src/views/Credentials/index.vue
+++ dashboard/src/views/Credentials/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="div flex justify-center w-full h-28 bg-brand-main">
+  <div class="flex justify-center w-full h-28 bg-brand-main">
     <header-logged />
   </div>
 
   <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
     <h1 class="text-4xl font-black text-center text-gray-800">Credenciais</h1>
     <p class="text-lg text-center text-gray-800 font-regular">
-      guia de instalação e geração de suas credenciais
+      Guia de instalação e geração de suas credenciais
     </p>
   </div>
 
-  <div class="flex justify-center w-full">
+  <div class="flex justify-center w-full h-full">
     <div class="flex flex-col w-4/5 max-w-6xl py-10">
       <h1 class="text-3xl font-black text-brand-darkgray">
         Instalação e configuração
@@ -25,7 +25,6 @@
         width="600px"
         height="50px"
       />
-
       <div
         v-else
         class="
@@ -41,11 +40,9 @@
           lg:w-1/2
         "
       >
-        <span v-if="state.hasError">Erro ao carregar a apiKey</span>
-        <span v-else id="apikey">
-          {{ store.User.currentUser.apiKey }}
-        </span>
-        <div class="flex ml-20 mr-1">
+        <span v-if="state.hasError">Erro ao carregar a apikey</span>
+        <span v-else id="apikey">{{ store.User.currentUser.apiKey }}</span>
+        <div class="flex ml-20 mr-5" v-if="!state.hasError">
           <icon
             @click="handleCopy"
             name="copy"
@@ -55,7 +52,7 @@
           />
           <icon
             id="generate-apikey"
-            @click="handleGenerateApiKey"
+            @click="handleGenerateApikey"
             name="loading"
             :color="brandColors.graydark"
             size="24"
@@ -65,7 +62,7 @@
       </div>
 
       <p class="mt-5 text-lg text-gray-800 font-regular">
-        Coloque o script abaixo no seu site para começar a receber feedback
+        Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
 
       <content-loader
@@ -74,11 +71,11 @@
         width="600px"
         height="50px"
       />
-
       <div
         v-else
         class="
           py-3
+          px-5
           pr-20
           mt-2
           rounded
@@ -89,10 +86,8 @@
         "
       >
         <span v-if="state.hasError">Erro ao carregar o script</span>
-        <pre v-else class="whitespace-nowrap ml-5">
-          &lt;script src="https://marqueslu-feedbacker-widget.netlify.app/{{
-            store.User.currentUser.apiKey
-          }}"&gt;&lt;/script&gt;
+        <pre v-else>
+&lt;script defer async onload="init('{{ store.User.currentUser.apiKey }}')" src="https://marqueslu-feedbacker-widget.netlify.app/init.js"&gt;&lt;/script&gt;
         </pre>
       </div>
     </div>
@@ -123,7 +118,7 @@ export default {
     watch(
       () => store.User.currentUser,
       () => {
-        if (!!store.Global.isLoading && !store.User.currentUser.apiKey) {
+        if (!store.Global.isLoading && !store.User.currentUser.apiKey) {
           handleError(true)
         }
       }
@@ -134,8 +129,21 @@ export default {
       state.hasError = !!error
     }
 
+    async function handleGenerateApikey () {
+      try {
+        state.isLoading = true
+        const { data } = await services.users.generateApikey()
+
+        setApiKey(data.apiKey)
+        state.isLoading = false
+      } catch (error) {
+        handleError(error)
+      }
+    }
+
     async function handleCopy () {
       toast.clear()
+
       try {
         await navigator.clipboard.writeText(store.User.currentUser.apiKey)
         toast.success('Copiado!')
@@ -144,21 +152,10 @@ export default {
       }
     }
 
-    async function handleGenerateApiKey () {
-      try {
-        state.isLoading = true
-        const { data } = await services.users.generateApiKey()
-        setApiKey(data.apiKey)
-        state.isLoading = false
-      } catch (error) {
-        handleError(error)
-      }
-    }
-
     return {
       state,
       store,
-      handleGenerateApiKey,
+      handleGenerateApikey,
       handleCopy,
       brandColors: palette.brand
     }
