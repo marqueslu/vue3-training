91363b458afac6109413cd9c2d210598310ccad1
Lucas Marques <lucas.silva@dextra-sw.com>
2022-01-25 15:25:34 -0300

    Add tests

--- widget/src/views/Playground/index.vue
+++ widget/src/views/Playground/index.vue
@@ -3,25 +3,24 @@
     class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main"
   >
     <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
-    <p class="text=2xl mt-3 font-regular text-brand-gray">Este é o playgeound, use para testar o widget.</p>
+    <p class="text-2xl mt-3 font-regular text-brand-gray">
+      Este é o playground, use para testar o widget.
+    </p>
   </div>
-
   <div
     class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray"
   >
-    <h1 class="text-6xl font-black text-brand-graydark">Playground</h1>
+    <h1 class="text-6xl font-black text-brand-graydark">🚀</h1>
   </div>
-
   <div
     class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main"
   >
-    <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
+    <h1 class="text-6xl font-black text-brand-gray">👀</h1>
   </div>
-
-   <div
+  <div
     class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray"
   >
-    <h1 class="text-6xl font-black text-brand-graydark">Playground</h1>
+    <h1 class="text-6xl font-black text-brand-graydark">🚨</h1>
   </div>
   <widget />
 </template>
